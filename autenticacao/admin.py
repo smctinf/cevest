e45f9a02376d9a7d6bd6dfaf66df976e28db2780
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2023-09-05 13:25:32 -0300

    05.09.23.1.ED

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Pessoa
 
 # Register your models here.
-admin.site.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'telefone', 'dt_nascimento', 'bairro', 'endereco', 'numero', 'complemento', 'cep', 'dt_inclusao')
+    search_fields = ('nome', 'email', 'cpf')
+    list_per_page = 20
+
+admin.site.register(Pessoa, PessoaAdmin)
